1c64d8b744c11865d7cdd6b0872be9d975e0780c
DESKTOP-EDHC096\Thiha Htun Naing <thihabisu1208@gmail.com>
2021-12-26 19:45:32 +0900

    first commit

---
+++ components/index.js
@@ -0,0 +1,19 @@
+export { default as PostCard } from "./PostCard";
+export { default as FeaturedPostCard } from "./FeaturedPostCard";
+
+// タブ
+export { default as PostWidget } from "./PostWidget";
+export { default as Categories } from "./Categories";
+
+// 詳細
+export { default as PostDetail } from "./PostDetail";
+export { default as Author } from "./Author";
+export { default as CommentsForm } from "./CommentsForm";
+export { default as Comments } from "./Comments";
+
+// レイアウト
+export { default as Header } from "./Header";
+export { default as Layout } from "./Layout";
+
+// ローダー
+export { default as Loader } from "./Loader";
